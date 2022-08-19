014a23c02263ddef67f4d609921c535129480927
tom <thomcdixon@gmail.com>
2022-08-18 19:03:31 -0700

    load model, classify and add TF logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
+import { useEffect, useState } from 'react'
 import logo from './logo.svg';
+import tsLogo from './TFLogo.svg';
+import * as toxicity from '@tensorflow-models/toxicity'
 import './App.css';
 
 function App() {
+
+  const [ modelLoading, setModelLoading ] = useState( false )
+  const [ classifying, setClassifying ] = useState( false )
+  const [ sentence, setSentence ] = useState( "Sally is nice" )
+  const [ threshold, setThreshold ] = useState( 0.85 )
+
+  useEffect( () => {
+    setModelLoading( true )
+    toxicity.load( threshold ).then( ( model ) => {
+      setModelLoading( false )
+      setClassifying( true )
+      model.classify( sentence ).then( ( predictions ) => {
+        // semi-pretty-print results
+        setClassifying( false )
+        console.log( JSON.stringify( predictions, null, 2 ) );
+      } );
+    } );
+  }, [ setModelLoading, setClassifying, sentence, threshold ] )
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={tsLogo} alt='TensorFlow logo' />
+        {modelLoading && <>        <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Model Loading
+          </p></>}
+        {classifying && <p>Classifying</p>}
       </header>
     </div>
   );
